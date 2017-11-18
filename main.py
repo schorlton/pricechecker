f1cd7964fcb5f5fa0264c6a06f49fff014c2acdd
Sam <chorltsd@mcmaster.ca>
2017-11-18 23:28:31 +0000

    Added email function

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ from contact import EMAIL_SENDER, EMAIL_RECIPIENT
 appengine.monkeypatch()
 
 def send_price_alert(price):
-    # mail.send_mail(
-    #     to=EMAIL_RECIPIENT,
-    #     subject='HeliJet Price Alert',
-    #     sender=EMAIL_SENDER,
-    #     body='There are flights available for ${}'.format(price)
-    # )
-    print('would have sent price alert for {}'.format(price))
+    mail.send_mail(
+        to=EMAIL_RECIPIENT,
+        subject='HeliJet Price Alert',
+        sender=EMAIL_SENDER,
+        body='There are flights available for ${}'.format(price)
+    )
+    # print('would have sent price alert for {}'.format(price))
 
 class PriceCheck(webapp2.RequestHandler):
     def get(self):
@@ -32,7 +32,7 @@ class PriceCheck(webapp2.RequestHandler):
         for price_a in price_as:
             prices.append(float(price_a.find('span').text[1:]))
         min_price = min(prices)
-        if min_price < 400:
+        if min_price < 200:
             send_price_alert(min_price)
 
 app = webapp2.WSGIApplication([
